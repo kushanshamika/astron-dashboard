b62eeabdfe6ba0ab6bfa2902ee553f2c8a398142
Kushan Shamika <shamikakushan@gmail.com>
2022-03-06 20:30:34 +0530

    💄 Vuetify

---
+++ src/components/NavBar.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <v-app-bar
+        app
+        clipped-left
+        color="deep-purple"
+        dark
+    >
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer
+        absolute
+        permanent
+    >
+      <template v-slot:prepend>
+        <v-list-item two-line>
+          <v-list-item-avatar>
+            <img src="https://randomuser.me/api/portraits/women/81.jpg" alt="">
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>Jane Smith</v-list-item-title>
+            <v-list-item-subtitle>Logged In</v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+
+      <v-divider></v-divider>
+
+      <v-list dense>
+        <v-list-item
+            v-for="item in items"
+            :key="item.title"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data: () => ({
+    items: [
+      { title: 'Home', icon: 'mdi-home-city' },
+      { title: 'My Account', icon: 'mdi-account' },
+      { title: 'Users', icon: 'mdi-account-group-outline' },
+    ],
+  }),
+}
+</script>
+
+<style scoped>
+
+</style>
